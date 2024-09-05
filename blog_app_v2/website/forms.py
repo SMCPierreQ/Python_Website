5401a2646c6738fc8d2e9664f0f4cc423ce8158e
SMCPierreQ <131826797+SMCPierreQ@users.noreply.github.com>
2024-09-05 18:55:52 +1200

    Update Account Part 2
    Now everything works as normal, committing to the GitHub repository as there are no errors.

--- blog_app_v2/website/forms.py
+++ blog_app_v2/website/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from flask_login import current_user 
 from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user 
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from .models import User
@@ -35,6 +35,7 @@ class UpdateAccountForm(FlaskForm):
                 raise ValidationError('That username is taken. Please choose a different one. ')
         
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one. ')
--- blog_app_v2/website/views.py
+++ blog_app_v2/website/views.py
@@ -2,9 +2,10 @@ import os
 from pathlib import Path
 from PIL import Image
 import secrets
-from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
+from flask import Blueprint, render_template, request, flash, redirect, url_for, current_app
 from flask_login import login_required, current_user
 from .models import Post, User, Comment
+from .forms import RegistrationForm, UpdateAccountForm
 from. import db
 
 views = Blueprint("views", __name__)
@@ -45,8 +46,9 @@ def posts(username):
     if not user:
         flash('No user with that username exists.', category="error")
         return redirect(url_for('views.home'))
-    
-    posts = Post.query.filter_by(author=user.id).all()
+    #Paginate pages
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.filter_by(author=user.id).paginate(page=page, per_page=4)
     return render_template("posts.html", user=current_user, posts=posts, username=username)
 
 
@@ -96,12 +98,32 @@ def delete_comment(comment_id):
     return redirect(url_for('views.blog'))
 
 def save_picture(form_picture):
-    path = Path("blog_app/static/profile_pics")
-    random_hex=secrets.token.hex(8)
+    root_path = current_app.root_path
+    random_hex = secrets.token_hex(8)
     _,f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(path, picture_fn)
-    output_size = (125, 125,)
-    i = Image.open(form_picture)
-    i.thumbnail(output_size)
-    i.save(picture_path)
+    picture_path = os.path.join(root_path, 'static', 'profile_pics', picture_fn)
+    output_size = (125, 125)
+    img = Image.open(form_picture)
+    img.thumbnail(output_size)
+    img.save(picture_path)
+    return picture_fn
+
+@views.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated.')
+        return redirect(url_for('views.account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', user=current_user, image_file=image_file, form=form)
