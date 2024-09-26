24f81b83ec3e7e847b017204f9589bf95c8233dd
SMCPierreQ <131826797+SMCPierreQ@users.noreply.github.com>
2024-09-26 13:19:57 +1200

    Save progress V5 part 1
    just in case

--- versions/blog_app_v5/website/auth.py
+++ versions/blog_app_v5/website/auth.py
@@ -11,27 +11,27 @@ auth = Blueprint("auth",__name__)
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('views.home'))
     if request.method == "POST":
         email = request.form.get("email")
         password = request.form.get("password")
-
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Logged in succesfully!', category='success')
+                flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
             flash('Email does not exist.', category='error')
-
     return render_template("login.html", user=current_user)
 
 @auth.route("/signup", methods=['GET', 'POST'])
 def sign_up():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('views.home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = generate_password_hash((form.password.data), method='pbkdf2:sha256')
