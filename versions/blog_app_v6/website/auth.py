ec12f3e8e0b72c84d1331ef2b47a4cb0107a8a9c
SMCPierreQ <131826797+SMCPierreQ@users.noreply.github.com>
2024-09-26 23:47:45 +1200

    Version Control v6
    Version Control v6

---
+++ versions/blog_app_v6/website/auth.py
@@ -0,0 +1,52 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from .import db
+from .models import User
+from flask_login import login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from .forms import RegistrationForm
+
+
+auth = Blueprint("auth",__name__)
+
+
+@auth.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        flash('You are logged in, to access this page you must log out. ', category='error')
+        return redirect(url_for('views.home'))
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+    return render_template("login.html", user=current_user)
+
+@auth.route("/signup", methods=['GET', 'POST'])
+def sign_up():
+    if current_user.is_authenticated:
+        flash('You are logged in, to access this page you must log out. ', category='error')
+        return redirect(url_for('views.home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash((form.password.data), method='pbkdf2:sha256')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created!', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template('signup.html', form=form, user=current_user)
+
+
+@auth.route("/logout", methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("auth.login"))
