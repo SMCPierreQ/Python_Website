30c431b1f68e7189bc1c6c662b4c18b514476a45
SMCPierreQ <131826797+SMCPierreQ@users.noreply.github.com>
2024-09-27 10:18:46 +1200

    Added comments
    Added comments to necessary files.

--- versions/blog_app_v6/website/__init__.py
+++ versions/blog_app_v6/website/__init__.py
@@ -9,7 +9,9 @@ db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
+    #Create flask application and define static folder for app
     app = Flask(__name__, static_folder='static')
+    #Secret key for app configuration
     app.config['SECRET_KEY'] = "lNJrn@]@neV]O.h"
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
@@ -22,13 +24,16 @@ def create_app():
 
     from .models import User, Post, Comment
 
+    # Create database if it doesn't exist
     with app.app_context():
         db.create_all()
 
+    # Initialize LoginManager for user session
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
+    #Loads user using login manager from the database using their ID
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
--- versions/blog_app_v6/website/forms.py
+++ versions/blog_app_v6/website/forms.py
@@ -6,42 +6,48 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 from .models import User
 
 class RegistrationForm(FlaskForm):
+    #Form for user registration
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    #Validates username to check if it is taken
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user: 
             raise ValidationError('That username is taken. Please choose a different one.')
-        
+    
+    #Validates email to check if it is taken
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
         
 class UpdateAccountForm(FlaskForm):
+    #Form for updating user information
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     picture = FileField('Update Profile Picture', validators= [FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
+    #Validates username to check if it is taken
     def validate_username(self, username):
         if username.data != current_user.username:
             user = User.query.filter_by(username=username.data).first()
             if user:
                 raise ValidationError('That username is taken. Please choose a different one. ')
-        
+    
+    #Validates email to check if it is taken
     def validate_email(self, email):
         if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is taken. Please choose a different one. ')
             
-
 class PostForm(FlaskForm):
+    #Form for creating or updating posts
     title = StringField('Title', validators=[DataRequired()])
     text = TextAreaField('Text', validators=[DataRequired()])
     submit = SubmitField('Update')
